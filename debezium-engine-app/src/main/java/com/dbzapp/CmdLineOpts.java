d2e804ea54ce521efb9facac8ed0cba89d5ff0c6
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-05-16 17:10:58 +0800

    Cleaned up, added defaults, showing help

--- debezium-engine-app/src/main/java/com/dbzapp/CmdLineOpts.java
+++ debezium-engine-app/src/main/java/com/dbzapp/CmdLineOpts.java
@@ -1,67 +1,95 @@
 package com.dbzapp;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Options;
 
 /**
  * Helper class to parse the command line options.
- * 
+ *
  * @author Sumukh Phalgaonkar, Vaibhav Kushwaha (vkushwaha@yugabyte.com)
  */
 public class CmdLineOpts {
   private final String connectorClass = "io.debezium.connector.yugabytedb.YugabyteDBConnector";
-  public String masterAddresses;
-  public String hostname;
-  public String databasePort = "5433";
-  public String streamId;
-  public String tableIncludeList;
-  public String databaseName = "yugabyte";
-  public String databasePassword = "Yugabyte@123";
-  public String databaseUser = "yugabyte";
-  public String snapshotMode = "never";
+  private String masterAddresses = "127.0.0.1:7100";
+  private String hostname = "127.0.0.1";
+  private String databasePort = "5433";
+  private String streamId;
+  private String tableIncludeList;
+  private String databaseName = "yugabyte";
+  private String databasePassword = "";
+  private String databaseUser = "yugabyte";
+  private String snapshotMode = "never";
+  private String offsetFile = Paths.get(System.getProperty("java.io.tmpdir"), "offset").toString();
 
-  public static CmdLineOpts createFromArgs(String[] args) {
+  public CmdLineOpts(String[] args){
+    Options opts = prepareOptions();
+    CommandLine commandLine = parseCommandList(opts, args);
+    initialize(commandLine);
+  }
+  private Options prepareOptions(){
     Options options = new Options();
 
-    options.addOption("master_addresses", true, "Addresses of the master process");
-    options.addOption("stream_id", true, "DB stream ID");
-    options.addOption("table_include_list", true, "The table list to poll for in the form"
-                      + " <schemaName>.<tableName>");
+    options.addOption(null, "master_addresses", true, "Addresses of the master process. Default: " + masterAddresses);
+    options.addRequiredOption("s", "stream_id", true, "(Required) DB stream ID");
+    options.addRequiredOption("t", "table_include_list", true, "(Required) The table list to poll for in the form <schemaName>.<tableName>");
+    options.addOption("d", "database", true, "Database name. Default: " + databaseName);
+    options.addOption("h", "hostname", true, "Database Hostname. Default: " + hostname );
+    options.addOption("p", "port", true, "Database Port. Default: " + databasePort );
+    options.addOption("U", "user", true, "Database user. Default: " + databaseUser );
+    options.addOption("W", "password", true, "Database password. Default: " + (databasePassword.equals("")?"<empty>":databasePassword));
+    options.addOption(null, "snapshot_mode", true, "Snapshot Mode. Default: " + snapshotMode);
+    options.addOption("f", "offset_file", true, "Offset file. Default: " + offsetFile);
+    options.addOption( null, "help", false, "Prints help (this screen)");
+    return options;
+  }
+  private CommandLine parseCommandList(Options options, String [] args){
 
     CommandLineParser parser = new DefaultParser();
     CommandLine commandLine = null;
     try {
       commandLine = parser.parse(options, args);
+      if (commandLine.hasOption("help")){
+        printHelp(options);
+        System.exit(0);
+      }
+    } catch(MissingOptionException e) {
+      printHelp(options);
+      System.exit(-1);
     } catch (Exception e) {
-      System.out.println("Exception while parsing arguments: " + e);
+      System.out.println(e.getMessage());
+      printHelp(options);
       System.exit(-1);
     }
-
-    CmdLineOpts configuration = new CmdLineOpts();
-    configuration.initialize(commandLine);
-    return configuration;
+    return commandLine;
+  }
+  private static void printHelp(Options options){
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp(App.class.getName(), options);
   }
 
-  private void initialize(CommandLine commandLine) {
-    if (commandLine.hasOption("master_addresses")) {
-      masterAddresses = commandLine.getOptionValue("master_addresses");
-      String[] nodes = masterAddresses.split(",");
-      hostname = nodes[0].split(":")[0];
-    }
-
-    if (commandLine.hasOption("stream_id")) {
-      streamId = commandLine.getOptionValue("stream_id");
-    }
-
-    if (commandLine.hasOption("table_include_list")) {
-      tableIncludeList = commandLine.getOptionValue("table_include_list");
-    }
+  private void initialize(CommandLine cmd) {
+    masterAddresses = cmd.getOptionValue("master_addresses", masterAddresses);
+    streamId = cmd.getOptionValue( "stream_id");
+    tableIncludeList = cmd.getOptionValue( "table_include_list");
+    hostname =cmd.getOptionValue( "hostname", hostname);
+    databasePort = cmd.getOptionValue( "port", databasePort );
+    databaseName = cmd.getOptionValue( "database", databaseName );
+    databaseUser = cmd.getOptionValue( "user", databaseUser );
+    databasePassword = cmd.getOptionValue( "password", databasePassword );
+    snapshotMode = cmd.getOptionValue( "snapshot_mode", snapshotMode );
+    offsetFile = cmd.getOptionValue( "offset_file", offsetFile);
   }
-
+  public String getOffsetFile() {
+    return offsetFile;
+  }
   public Properties asProperties() {
     Properties props = new Properties();
     props.setProperty("connector.class", connectorClass);
@@ -76,6 +104,10 @@ public class CmdLineOpts {
     props.setProperty("database.dbname", databaseName);
     props.setProperty("database.server.name", "dbserver1");
     props.setProperty("snapshot.mode", snapshotMode);
+    props.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
+    props.setProperty("offset.storage.file.filename", offsetFile);
+    props.setProperty("name", "engine");
+    props.setProperty("offset.flush.interval.ms", "60000");
 
     return props;
   }
