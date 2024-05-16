fda6a637f5b555e06ccd8d268bc91bc5b405a5bd
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-05-16 17:10:38 +0800

    Update to use CmdLieOpts via constructor

--- debezium-engine-app/src/main/java/com/dbzapp/App.java
+++ debezium-engine-app/src/main/java/com/dbzapp/App.java
@@ -2,14 +2,14 @@ package com.dbzapp;
 
 /**
  * Sample application class to demonstrate working of Debezium Embedded Engine with YugabyteDB.
- * 
+ *
  * @author Sumukh Phalgaonkar, Vaibhav Kushwaha (vkushwaha@yugabyte.com)
  */
 public class App {
   public static void main(String[] args) {
     System.out.println("Starting embedded application to run DBZ Embedded engine with Yugabyte");
 
-    CmdLineOpts configuration = CmdLineOpts.createFromArgs(args);
+    CmdLineOpts configuration = new CmdLineOpts(args);
     try {
       EngineRunner engineRunner = new EngineRunner(configuration);
       engineRunner.run();
